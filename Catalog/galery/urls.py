d63ca9525b9d217c3c34b3cb7da00ee8740639f5
Admin <dazovsckyao@gmail.com>
2024-04-10 22:13:38 +0300

    first commit

--- Catalog/galery/urls.py
+++ Catalog/galery/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.gallery_view, name='gallery'),
+    path('process_letter/', views.letters_view, name='letters_view'),
+    path('process_letter/<str:letter>/', views.process_letter, name='process_letter'),
 ]
--- Catalog/galery/views.py
+++ Catalog/galery/views.py
@@ -1,1 +1,24 @@
-from django.shortcuts import render, redirect
+from unittest import loader
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+
+def index(request):
+    latest_question_list = Case.objects.all()
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "galery/gallery.html", context)
+
+
+def letters_view(request):
+    letters = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
+    return render(request, 'galery/gallery.html', {'letters': letters})
+
+
+def process_letter(request, letter):
+    filtered_cases = Case.objects.filter(name__istartswith=letter)
+    list_author = list(filtered_cases.values_list('name', flat=True))
+    return render(request, 'galery/gallery.html', {'cases': list_author})
+
+
+
